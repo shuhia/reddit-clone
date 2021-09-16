5f657c416c45347114bbf8c9618fe3dac40b3fc5
Alex On <shuhia.on@gmail.com>
2021-09-16 19:00:36 +0200

    put assets in assets folder

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,10 +1,10 @@
 import React from "react";
 
-import bigLogo from "./reddit_logo_horizontal_on_orangered.png";
-import personIcon from "./person_black_24dp.svg";
-import arrowDownIcon from "./keyboard_arrow_down_black_24dp.svg";
-import searchIcon from "./search_black_24dp.svg";
-import clearIcon from "./clear_black_24dp.svg";
+import bigLogo from "./assets/reddit_logo_horizontal_on_orangered.png";
+import personIcon from "./assets/person_black_24dp.svg";
+import arrowDownIcon from "./assets/keyboard_arrow_down_black_24dp.svg";
+import searchIcon from "./assets/search_black_24dp.svg";
+import clearIcon from "./assets/clear_black_24dp.svg";
 
 import "./Header.css";
 
